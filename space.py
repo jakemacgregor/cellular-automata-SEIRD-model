9a60f753713b51a49e012e84e9e70b186ef69a12
Jake MacGregor <45104207+jakemacgregor@users.noreply.github.com>
2023-01-30 20:04:11 +0000

    Remove 'get neighbourhood' function

--- space.py
+++ space.py
@@ -8,15 +8,3 @@ class Space:
         self.c = c
 
         self.cells = [[] * c]
-
-    def get_neighbourhood(self, cell_coords):
-        cell = self.cells[cell_coords[0][cell_coords[1]]]
-        neighbours = [[], [], []]
-
-        for i in range(-1, 2):
-            if cell_coords[0] + i < 0 or cell_coords[0] + i >= self.r:
-                continue
-            for j in range(-1, 2):
-                if cell_coords[0] + j < 0 or cell_coords[0] + j >= self.c:
-                    continue
-                neighbours[(i+1)[j+1]] = [cell_coords[0] + i, cell_coords[1] + j]
