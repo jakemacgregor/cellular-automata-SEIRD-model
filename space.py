74f69a0923343530076ece9cd9196bf14d02a79d
Jake MacGregor <45104207+jakemacgregor@users.noreply.github.com>
2023-01-31 07:52:50 +0000

    Improve space class

--- space.py
+++ space.py
@@ -1,5 +1,7 @@
 from cell import Cell
 
+neighbours = [["NW", "N", "NE"], ["W", "X", "E"], ["SW", "S", "SE"]]
+
 
 class Space:
     def __init__(self, r, c):
@@ -7,4 +9,22 @@ class Space:
         self.r = r
         self.c = c
 
-        self.cells = [[] * c]
+        cells = []
+        for j in range(c):
+            cells.append([])
+
+        self.cells = cells
+
+    def get_neighbourhood(self, coords):
+        neighbourhood = {}
+
+        for i in range(-1, 2):
+            if not 0 <= coords[0] + i < self.r:
+                continue
+            for j in range(-1, 2):
+                if not 0 <= coords[1] + j < self.c:
+                    continue
+                n = neighbours[i][j]
+                neighbourhood[n] = [i, j]
+
+        return neighbourhood
