5aebf6ced89188747bde5be72593b78187353b9e
Jake MacGregor <45104207+jakemacgregor@users.noreply.github.com>
2023-02-06 16:07:01 +0000

    Correct parameters and update results

--- simulation.py
+++ simulation.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     columns = 50
     rows = 50
     eps = 0.4
-    vir = 0.3
+    vir = 0.6
     iterations = 50
     output_timestamps = [0, 5, 10, 15, 20, 25]
 
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         columns = int(input("Number of columns (int):") or "50")
         rows = int(input("Number of rows (int):") or "50")
         eps = float(input("Epsilon value (float):") or "0.4")
-        vir = float(input("Virulence (float):") or "0.3")
+        vir = float(input("Virulence (float):") or "0.6")
         iterations = int(input("Number of iterations (int):") or "50")
 
     space = Space(columns, rows, eps, vir)
